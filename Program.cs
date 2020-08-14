687ee9be0510db67d9e873140f10fc305a6cfbdb
unknown <ayeshafatima977@gmail.com>
2020-08-14 14:32:13 -0600

    Added docstring at the top of program;

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*Title:Create,Read,Update and Delete the data set
+Purpose of the Project: An application that will maintain a dataset of items and allow the user to manipulate that dataset.
+Author:Ayesha Fatima
+Last Modified:15th August,2020
+*/
+
+
+using System;
 
 namespace c_assignment_crud_ayeshafatima977
 {
