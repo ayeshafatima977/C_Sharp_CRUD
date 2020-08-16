cf7812712dd8198cd843aca30a1ad4517ec293e2
unknown <ayeshafatima977@gmail.com>
2020-08-16 16:32:44 -0600

    Using Lists:Created Menu to Add,Read,Exit the Menu;

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ Last Modified:15th August,2020
 */
 
 using System;
+using System.Collections.Generic;
 
 namespace c_assignment_crud_ayeshafatima977
 {
@@ -12,117 +13,89 @@ namespace c_assignment_crud_ayeshafatima977
     {
         static void Main(string[] args)
         {
-            //------------
-            //Prompting the user application purpose and how it can be operated.
-            //------------
+            List<int> numbers = new List<int>(10);//Limits the list capacity by 10 numbers
+            List<string> key = new List<string>();
+            int menuOption = -1;
+            while (menuOption != 0)
 
-            Console.WriteLine("Welcome!This application will prompt you to create a data set which can be read,updated and modified");
-
-            //------------
-            //Declaring variables
-            //------------
-
-            int[] inputArray = new int[10], storeArray = new int[10], outputArray = new int[10];
-    
-            int menuOption;
-        
-            //-------------
-            //MenuOption Selection
-            //--------------
-        do
             {
-                
-                Console.WriteLine("Menu\n------\n1.Add \n2.Read \n3.Exit");
+
+                Console.WriteLine("Menu\n------\n1.Add \n2.Read");
                 Console.Write("Please enter a menu option:");
                 menuOption = int.Parse(Console.ReadLine());
-                switch(menuOption) 
+                switch (menuOption)
                 {
 
                     case 1:
-                    Console.WriteLine("Running option {0}",menuOption);
-                    break;
-                    case 2: 
-                    Console.WriteLine("Running option {0}",menuOption);
-                    break;
-                    case 3:
-                    Console.WriteLine("Exiting program");
-                    break;
-                    default :
-                    Console.WriteLine("Invalid option selected.");
-                    break;
+                        PopulateList(numbers);  //Calling input Array for userInput for dataset
+                        break;
+                    case 2:
+                        Output(numbers);
+                        break;
+
+                    /* case 3:
+                     EditList(numbers);
+                     break;*/
+
+                    default:
+                        Console.WriteLine(" Invalid Input selected; Press Enterkey to exit");
+                        Exit(key);
+
+                        break;
                 }
+            }
 
-            }while(menuOption==2 && menuOption!= 3);//When menuoption is not equal to start filling the data set to 10 values to display the output automatically
-            
-            //Calling output Array
-
-            PopulateArray(inputArray);  //Calling input Array for userInput for dataset
         }
-        //-----------
-        //Adding elements to Array
-        //------------
-        static void PopulateArray(int[] Array)
+
+        static void PopulateList(List<int> inputStore)
         {
-            string userInput;// Required for sentineval value
-            int storeArray = 0;//The no. of elements that we actually care about in the array
-            int[] inputArray = new int[10];
+            string userInput;
             do
             {
-                
-            
-                int userInputInt;
-                Console.Write("Please enter a integer, or \"quit\" to output: ");
+                Console.Write("Please enter a integer, or \"done\" to finish the list: ");
+
                 userInput = Console.ReadLine();
 
                 try
-
                 {
-                    // If this parse fails, none of the rest of the try block will execute, meaning we won't add garbage data
-                    // to our array, and the logical size will not go up.
-                    userInputInt = int.Parse(userInput); //Converting string to integer
-
-                    inputArray[storeArray] = userInputInt;
-                    storeArray++;
+                    inputStore.Add(int.Parse(userInput));
                 }
                 catch
                 {
-                    // To let user know wthey have entered an invalid input
-
-                    if (userInput.ToLower().Trim() != "quit")
+                    if (userInput.ToLower().Trim() != "done")
                     {
                         Console.WriteLine("You entered invalid input, please try again.");
                     }
                 }
+            } while (userInput.ToLower().Trim() != "done");
+        }
 
-                // Exit the program when the user enters a sentinel value Quit
+        // Display
 
-            } while (storeArray < inputArray.Length && userInput.ToLower().Trim() != "quit");
-
-            //----------------
-            //Displaying Output to the User:
-            //----------------
-             /*      static void Display(int[] Array)
-            {*/
-               // int storeArray = 0;
-              //  int[] inputArray = new int[10];
-                int[] outputArray = new int[10];
-
-                for (int i = 0; i < storeArray; i++)
-                {
-                    Console.Write("The dataset contains value of:");
-                    outputArray[i]=inputArray[i];
-                    Console.WriteLine(outputArray[i]); // Console.WriteLine(inputArray[i]);
-                }
-            
-            
- 
+        static void Output(List<int> theList)
+        {
+            for (int i = 0; i < theList.Count; i++)
+            {
+                Console.WriteLine(theList[i]);
+            }
 
         }
+        //Edit
+
+  /*      static void EditList(List<int> myList)
+        {
+            myList.Add(15);
+        }
+*/
+
+        // Exit
+        static void Exit(List<string> key)
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("\n Press enter key to end the program");
+            }
+        }
     }
+
 }
-
-
-
-
-
-
