1c8bd07632fc65ea91e54c103bf2e0b2a510dd6d
unknown <ayeshafatima977@gmail.com>
2020-08-16 20:10:28 -0600

    Added the methods to each menu;Formated/Indented the code;Added comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿/*Title:Creat1e,Read,Update and Delete the data set
+﻿
+/*Title:Creat1e,Read,Update and Delete the data set
 Purpose of the Project: An application that will maintain a dataset of items and allow the user to manipulate that dataset.
 Author:Ayesha Fatima
 Last Modified:15th August,2020
@@ -13,32 +14,50 @@ namespace c_assignment_crud_ayeshafatima977
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>(10);//Limits the list capacity by 10 numbers
+            //------------
+            //Prompting the user application purpose and how it can be operated.
+            //------------
+
+            Console.WriteLine("Welcome!This application will prompt you to create a data set which can be read,modified in later stages");
+
+            //------------
+            //Declaring variables
+            //------------
+
+            List<int> luckyNumbers = new List<int>(10);//Limits the list capacity by 10 numbers
             List<string> key = new List<string>();
             int menuOption = -1;
+
+
+            //-------------
+            // Creating a Menu for User to select between Adding or Read or Exit
+            //------------
+
             while (menuOption != 0)
 
             {
 
-                Console.WriteLine("Menu\n------\n1.Add \n2.Read");
+                Console.WriteLine("Menu\n------\n1.ADD \n2.READ.\n0.EXIT: Press Enter key");
                 Console.Write("Please enter a menu option:");
                 menuOption = int.Parse(Console.ReadLine());
+
                 switch (menuOption)
                 {
-
                     case 1:
-                        PopulateList(numbers);  //Calling input Array for userInput for dataset
+                        Console.WriteLine("ADD Option will allow you to enter maximum 10 luckyNumbers");
+
+                        PopulateList(luckyNumbers);
+
                         break;
                     case 2:
-                        Output(numbers);
+                        Console.WriteLine("READ Option will allow you to read the entered luckyNumbers");
+
+                        Output(luckyNumbers);
+
                         break;
-
-                    /* case 3:
-                     EditList(numbers);
-                     break;*/
-
                     default:
-                        Console.WriteLine(" Invalid Input selected; Press Enterkey to exit");
+                        Console.WriteLine("Invalid Input selected; Press Enterkey to exit");
+
                         Exit(key);
 
                         break;
@@ -47,22 +66,28 @@ namespace c_assignment_crud_ayeshafatima977
 
         }
 
+
+        //------------
+        // Add entries to the list
+        //------------
+
         static void PopulateList(List<int> inputStore)
         {
-            string userInput;
+            string userInput;// Required for sentineval value
             do
             {
-                Console.Write("Please enter a integer, or \"done\" to finish the list: ");
-
+                Console.Write("Please enter maximum 10 lucky numbers, or \"done\" to finish the list: ");// This allows the user to enter data until the array is full, and keep the program loop running until exited with a sentinel value of done is entered.
                 userInput = Console.ReadLine();
 
                 try
                 {
-                    inputStore.Add(int.Parse(userInput));
+
+                    inputStore.Add(int.Parse(userInput)); // If this parse fails, none of the rest of the try block will execute.
                 }
                 catch
                 {
-                    if (userInput.ToLower().Trim() != "done")
+
+                    if (userInput.ToLower().Trim() != "done") //Trim() function removes any leading whitespaces at the start or at the end;Lower() is used to prevent from case sensitive
                     {
                         Console.WriteLine("You entered invalid input, please try again.");
                     }
@@ -70,25 +95,24 @@ namespace c_assignment_crud_ayeshafatima977
             } while (userInput.ToLower().Trim() != "done");
         }
 
-        // Display
 
-        static void Output(List<int> theList)
+        //-------------
+        // Displays entries of the list
+        //------------
+
+        static void Output(List<int> myList)
         {
-            for (int i = 0; i < theList.Count; i++)
+            for (int i = 0; i < myList.Count; i++)
             {
-                Console.WriteLine(theList[i]);
+                Console.WriteLine($"Item#.{i + 1}:" + myList[i]);
             }
 
         }
-        //Edit
 
-  /*      static void EditList(List<int> myList)
-        {
-            myList.Add(15);
-        }
-*/
+        //-------------
+        // Exits of the menu
+        //------------
 
-        // Exit
         static void Exit(List<string> key)
         {
             while (Console.ReadKey().Key != ConsoleKey.Enter)
@@ -98,4 +122,13 @@ namespace c_assignment_crud_ayeshafatima977
         }
     }
 
+
 }
+
+
+
+
+
+
+
+
